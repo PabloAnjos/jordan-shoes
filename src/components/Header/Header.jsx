3dc312df481843e1c7e7dec0785c46133d22d46e
Pablo Anjos <pablomatheussilvaanjoss@gmail.com>
2024-10-10 14:40:14 -0300

    Header criado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
 import "../Header/Header.scss"
-
-
+import imgJordan from "../../assets/images/jordan.png"
+import logoJordan from "../../assets/images/logo-jordan.png"
 
 export function Header() {
     return (
         <>
             <header>
-                
+                    <h1>Frete grátis para todo o Brasil</h1>
+                 
+                    <div className="content-jordan-shoes">
+                        <h2>JordanShoes</h2>
+                        <h3>A melhor loja de Jordan</h3>
+                        <p>O tênis Jordan é fruto de uma velha e forte
+                            parceria entre a Nike e o jogador Michael Jordan.</p>
+                    </div>
+
+                    <img src={imgJordan} alt="Imagem jogador de basquete jordan" />
             </header>
         </>
     )
